a7453de109222b7bf798f0e92b92cbbc63e9be69
Moustafaa91 <moustafa.attia.91@gmail.com>
2024-07-18 00:19:58 +0100

    Initial commit

---
+++ routes/hello.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET hello page. */
+router.get('/:string', function(req, res) {
+  var string = req.params.string;
+  res.send('Hello ' + string);
+});
+
+module.exports = router;
