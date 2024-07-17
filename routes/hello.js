3b77c9a68921ebeaca82bb0d9804fb84be4e19e2
Moustafaa91 <moustafa.attia.91@gmail.com>
2024-07-18 00:40:05 +0100

    test CI/CD

--- routes/hello.js
+++ routes/hello.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET hello page. */
 router.get('/:string', function(req, res) {
   var string = req.params.string;
-  res.send('Hello ' + string);
+  res.send('TEST CI/CD Hello ' + string);
 });
 
 module.exports = router;
